eeb193a8adb6a85b95f72b49b1bf63c9f3c89e7c
Andrés Torres <125112555+andres-stg@users.noreply.github.com>
2024-02-22 05:36:24 +0000

    definidos pronoun, adj, noun y extension

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["The", "My", "Ours", "Hers"];
+  let adj = ["masterful", "prosmiscuous", "immeasurable", "magical"];
+  let noun = ["Volkswagen", "Programmer", "Virgin", "Railway"];
+  let extension = [".net", ".com", ".ve", ".io"];
+
+  function generateDomainName(arr1 = [], arr2 = [], arr3 = [], arr4 = []) {
+    let domainName = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+      for (let j = 0; j < arr2.length; j++) {
+        for (let k = 0; k < arr3.length; k++) {
+          for (let l = 0; l < arr4.length; l++)
+            domainName.push(arr1[i] + arr2[j] + arr3[k] + arr4[l]);
+        }
+      }
+    }
+  }
+  return domainName;
 };
